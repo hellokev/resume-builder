00b05ad0132fb08c2c519e6aabae5bd195987fdb
hellokev <2kevinloi8@gmail.com>
2024-02-08 13:38:44 -0800

    Removed unnecessary code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
 import BasicInfo from './components/BasicInfo';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BasicInfo />
