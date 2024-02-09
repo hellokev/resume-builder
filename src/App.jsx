e9151e8f1c697eacbb732675cf36afdff80934aa
hellokev <2kevinloi8@gmail.com>
2024-02-09 15:06:05 -0800

    Added Experience Component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import BasicInfo from './components/BasicInfo';
 import Education from './components/Education';
+import Experience from './components/Experience';
 
 function App() {
   return (
     <>
       <BasicInfo />
       <Education />
+      <Experience />
     </>
   )
 }
