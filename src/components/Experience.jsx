33648af673edff6f70607f4da4f03db64d6be5dc
hellokev <2kevinloi8@gmail.com>
2024-02-13 16:06:33 -0800

    added edit logic to Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,6 +7,7 @@ function Experience() {
     const [endDate, setEndDate] = useState('');
     const [desc, setDesc] = useState('');
     const [submit, setSubmit] = useState(null);
+    const [edit, setEdit] = useState(true);
 
     function handleCompany(e) {
         setCompanyName(e.target.value);
@@ -38,52 +39,67 @@ function Experience() {
             desc
         }
         setSubmit(formData);
+        setEdit(false);
+    }
+
+    function handleEdit() {
+        setEdit(true);
+    }
+
+    function handleCancelEdit() {
+        setEdit(false);
     }
 
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <input
-                placeholder="Company name"
-                value={companyName}
-                onChange={handleCompany}
-            />
-            <input 
-                placeholder="Position"
-                value={position}
-                onChange={handlePosition}
-            />
-            <input
-                placeholder="Start Date"
-                value={startDate}
-                onChange={handleStartDate}
-            />
-            <input
-                placeholder="End Date"
-                value={endDate}
-                onChange={handleEndDate}
-            />
-             <input
-                placeholder="Description"
-                value={desc}
-                onChange={handleDesc}
-            />
-            <button type='submit'>Submit</button>
-        </form>
-
-        {submit && (
-            <div>
-                <p>Company Name: {submit.companyName}</p>
-                <p>Position: {submit.position}</p>
-                <p>Start Date: {submit.startDate}</p>
-                <p>End Date: {submit.endDate}</p>
-                <p>Desc: {submit.desc}</p>
-            </div>
-        )}
-    </>
+            {edit ? (
+                <form onSubmit={handleSubmit}>
+                    <input
+                        placeholder="Company name"
+                        value={companyName}
+                        onChange={handleCompany}
+                    />
+                    <input 
+                        placeholder="Position"
+                        value={position}
+                        onChange={handlePosition}
+                    />
+                    <input
+                        placeholder="Start Date"
+                        value={startDate}
+                        onChange={handleStartDate}
+                    />
+                    <input
+                        placeholder="End Date"
+                        value={endDate}
+                        onChange={handleEndDate}
+                    />
+                    <input
+                        placeholder="Description"
+                        value={desc}
+                        onChange={handleDesc}
+                    />
+                    <button type='submit'>Submit</button>
+                    {submit && (
+                        <button type='button' onClick={handleCancelEdit}>Cancel Edit</button>
+                    )}
+                </form>
+            ) : (
+                <>
+                    {submit && (
+                        <div>
+                            <p>Company Name: {submit.companyName}</p>
+                            <p>Position: {submit.position}</p>
+                            <p>Start Date: {submit.startDate}</p>
+                            <p>End Date: {submit.endDate}</p>
+                            <p>Desc: {submit.desc}</p>
+                        </div>
+                    )}
+                    <button onClick={handleEdit}>Edit</button>
+                </>
+            )}
+        </>
     )
-
-
 }
 
 export default Experience;
